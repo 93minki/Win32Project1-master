5cefccf5075d05a7249e9fc002fc2e40fa8d4297
김민기 <93minki@taeha.co.kr>
2020-01-08 15:36:03 +0900

    8차 수정

--- Win32Project1/Win32Project1.cpp
+++ Win32Project1/Win32Project1.cpp
@@ -1,6 +1,7 @@
 // Win32Project1.cpp : 응용 프로그램에 대한 진입점을 정의합니다.
-// 7차 수정 (20.01.08)
-// -연산 문제
+// 8차 수정 (20.01.08)
+// -연산 문제 해결
+// -Clear 기능 수정
 
 #include "stdafx.h"
 #include "Win32Project1.h"
@@ -21,6 +22,7 @@ char InputNum[MAX_STACK_SIZE];
 char TempArray[MAX_STACK_SIZE];
 float StackNum[MAX_STACK_SIZE];									// Get Number in Stack
 char StackSymbol[MAX_STACK_SIZE];								// Get Sign in Stack
+char showfloat[MAX_STACK_SIZE];
 
 int np;
 int Search_n = 0;									// Search Number 
@@ -205,11 +207,13 @@ void InitTempArray() {
 	
 }
 
-void CheckFormula(HWND hDlg,int num) {
+bool CheckFormula(HWND hDlg,int num) {
 	// Show Error MessageBox if last stack element is Arithmetic Symbol
 	if (InputNum[num + 1] == '\0') {
 		MessageBox(hDlg, "Wrong formula!!!", "Formula Error", MB_OK);
+		return true;
 	}
+	return false;
 }
 
 void Calculation() {
@@ -259,8 +263,9 @@ int makePriority(char symbol) {
 	return priority;
 }
 
-void GetExp(HWND hDlg) {
 
+void ShowOutput(HWND hDlg) {
+	
 	printf("Get Number : %s\n", InputNum);
 	printf("Size of Stack : %d\n", GetStackSize());
 	int StackSize = GetStackSize();
@@ -269,45 +274,57 @@ void GetExp(HWND hDlg) {
 	int pop1;
 	int pop2;
 	bool calcFlag = false;															// 연산자 우선순위에 의해 먼저 연산해야 하는 경우 Flag를 사용해서 연산
+	bool shiftFlag = false;
 	int rst = 0;
 	char opr;
-
-	char showfloat[MAX_STACK_SIZE];
 
 	for (int i = 0; i < StackSize; i++) {
 		// if Stack Value is Sign 
 		if (InputNum[i] == '+' || InputNum[i] == '-' || InputNum[i] == '*' || InputNum[i] == '/') {
+			if (CheckFormula(hDlg,i)) {												// 잘못된 수식 검출 ex. +,-,*,/ 끝날 경우 3+ , 3+4+, 등
+				return;
+			}
 			pr = makePriority(InputNum[i]);											// 현재 연산자의 우선순위를 구한다.
-			Search_s = i;															// .
-			for (int tp = 0; Search_n < Search_s; Search_n++, tp++) {				// InputNum[i] 이전 숫자를 TempArray에 저장
+			Search_s = i;															// 탐색범위의 최대값을 현재 i값으로 저장 탐색범위 최소값은 초기에 0으로 설정되어 있다.
+			for (int tp = 0; Search_n < Search_s; Search_n++, tp++) {				// 최소 탐색범위 부터 최대 탐색범위까지 ( = 숫자)를 TempArray에 저장
 				TempArray[tp] = InputNum[Search_n];
 			}
-			Search_n = Search_s + 1;												// InputNum[i]는 연산기호 이기 때문에 그 다음 부터 찾는다.
-			NumStackPush(atof(TempArray));											// 피연산자 숫자로 변환하여 스택에 쌓는다.
-			InitTempArray();														// TempArray 초기화
-			if (calcFlag) {															
-				Calculation();								
+			Search_n = Search_s + 1;												// 탐색범위의 최소값을 연산기호 다음으로 설정 ex. 123+456+789 [0]~[3] [4(연산자)] [5] ~
+			if (shiftFlag) {
+				NumStackPush(atof(TempArray)*-1);									// -연산자와 -기호를 구분하기 위해서 식을 수정한다. 5-40 => 5 + (-40)
+				shiftFlag = false;			
+			}
+			else {
+				NumStackPush(atof(TempArray));										// 피연산자 숫자로 변환하여 스택에 쌓는다.
+			}
+			InitTempArray();														// atof를 사용해서 문자열을 숫자로 변환하고, TempArray는 초기화한다.
+			if (calcFlag) {															// 연산자 우선순위에 의해 먼저 계산해야 할 경우 Flag가 On되고 Flag에 따라 연산을 수행.
+				Calculation();
 				SymStackPush(InputNum[i]);
 				calcFlag = false;
 			}
-			else if (oldpr == 4) {													// 첫 번째 연산자.
+			else if (oldpr == 4) {													// 첫 번째 연산자. 연산자 우선순위 관계 없이 스택에 쌓여야 한다. (oldpr 초기값은 4로 처음을 구분하기 위한 숫자)
 				SymStackPush(InputNum[i]);											// 연산자를 스택에 쌓는다.
 				oldpr = pr;															// 기존 우선순위는 현재 우선순위가 된다.
 			}
 			else if (oldpr < pr) {													// 새로운 연산자 우선순위가 기존 우선순위 보다 높을 경우 */ -> +-
 				Calculation();														// 연산
 				SymStackPush(InputNum[i]);											// 연산 후 연산기호를 스택에 쌓는다.
-				oldpr = pr;
+				oldpr = pr;															// 기존 연산자 우선순위는 현재 연산자 우선순위로 대체된다.
 			}
 			else if (oldpr == pr) {													// 새로운 연산자 우선순위가 기존 우선순위와 같을 경우
-				Calculation();
-				SymStackPush(InputNum[i]);
-				oldpr = pr;
+				Calculation();														// 연산
+				SymStackPush(InputNum[i]);											// 연산 후 연산기호를 스택에 쌓는다.
+				oldpr = pr;															// 기존 연산자 우선순위는 현재 연산자 우선순위로 대체된다.
 			}
 			else if (oldpr > pr) {													// 새로운 연산자 우선순위가 기존 우선순위 보다 낮을 경우 +- -> */
-				SymStackPush(InputNum[i]);
-				calcFlag = true;
-				//oldpr = pr;
+				SymStackPush(InputNum[i]);											// 연산을 하지 않고 스택에 쌓아둔다.
+				calcFlag = true;													// 새로운 연산자가 쌓이기 전에 먼저 연산을 수행하도록 Flag를 On한다.
+				//oldpr = pr;														// 스택에 쌓이지 않고 바로 연산을 해야하기 때문에 연산자 우선순위는 그대로 유지
+			}
+			if (InputNum[i] == '-') {												// 탐색한 연산자가 '-' 일 경우 쌓여있던 연산자 '-'를 '+'로 교체한다.
+				StackSymbol[ssp] = '+';	
+				shiftFlag = true;													// Flag를 사용해서 이 다음에 들어오는 숫자는 -가 붙어서(음수가 되어) 스택에 쌓인다.
 			}
 		}
 
@@ -317,27 +334,25 @@ void GetExp(HWND hDlg) {
 			for (int tp = 0; Search_n < Search_s; Search_n++, tp++) {
 				TempArray[tp] = InputNum[Search_n];
 			}
-			NumStackPush(atof(TempArray));											// 피연산자를 스택에 쌓는다.
-			for(int x = 0 ; x<MAX_STACK_SIZE;x++){
-				Calculation();
-				if (IsSymStackEmpty()) {
-					InitTempArray();
-					printf("StackNum[0] : %f\n", StackNum[0]);
-					sprintf(showfloat, "%0.2f",StackNum[0]);
-					//SetDlgItemInt(hDlg, IDC_EDIT2, StackNum[0], TRUE);
-					SetDlgItemText(hDlg, IDC_EDIT2, showfloat);
-					fclean = true;
-					break;
-				}
+			if (shiftFlag) {
+				NumStackPush(atof(TempArray)*-1);
 			}
+			else {
+				NumStackPush(atof(TempArray));											// 피연산자를 스택에 쌓는다.
+			}
+			
+			for (int x = 0; !IsSymStackEmpty(); x++) {
+				Calculation();
+			}
+			InitTempArray();
+			printf("StackNum[0] : %f\n", StackNum[0]);
+			sprintf(showfloat, "%0.2f", StackNum[0]);
+			SetDlgItemText(hDlg, IDC_EDIT2, showfloat);
+			fclean = true;
+			break;
+
 		}
 	}
-}
-
-void ShowOutput(HWND hDlg) {
-	
-	GetExp(hDlg);
-
 }
 
 void RmArray(HWND hDlg) {
@@ -366,6 +381,12 @@ void ClearArray(HWND hDlg) {
 			break;
 		}
 		InputNum[a] = '\0';
+	}
+	for (int b = 0; b < sizeof(showfloat); b++) {
+		if (showfloat[b] == '\0') {
+			break;
+		}
+		showfloat[b] = '\0';
 	}
 	InputNum[0] = '0';
 	np = 0;
